05bed2a445921c095123926d9ba223d242382ff8
Maloney, Christopher <maloneyc@ncbi.nlm.nih.gov>
2014-03-02 01:24:41 -0500

    Initial commit

---
+++ do.sh
@@ -0,0 +1,4 @@
+java -cp $SAXON95/saxon9he.jar:$SAXON95/xml-commons-resolver-1.2/resolver.jar \
+  net.sf.saxon.Transform \
+  -catalog:catalog/catalog.xml -t \
+  -s:fleegle.xml -xsl:fleegle.xsl -o:fleegle.new
